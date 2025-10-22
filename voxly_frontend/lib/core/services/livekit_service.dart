a119f085efdeba5affcb5f07aa7196c694d4b4c1
Alex <43788117+Developer1414@users.noreply.github.com>
2025-10-22 14:09:16 +0300

    fix

--- voxly_frontend/lib/core/services/livekit_service.dart
+++ voxly_frontend/lib/core/services/livekit_service.dart
@@ -56,11 +56,11 @@ class LivekitService extends ChangeNotifier {
       socket = IO.io(
         kDebugMode
             ? 'http://localhost:3000'
-            : 'wss://voxly-audio.ru/ws', //'https://voxly-backend.onrender.com',
+            : 'https://voxly-audio.ru', //'https://voxly-backend.onrender.com',
         IO.OptionBuilder()
             .setTransports(['websocket', 'polling'])
             .disableAutoConnect()
-            .setPath('/ws/socket.io/')
+            .setPath('/socket.io/')
             .build(),
       );
 
